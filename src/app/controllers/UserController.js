6dd7f93c2ffacb0351e6786d90c5414be4302205
pedro <pedroh.fortunatos@gmail.com>
2019-11-08 17:50:52 -0300

    at

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,38 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists.' });
+    }
+
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+    const user = await User.findByPk(req.userId);
+    console.log(user.email);
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+    return res.json({
+      ok: true,
+      a: req.userId,
+    });
+  }
+}
+
+export default new UserController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '0b6e05457c5eaff4a918cbaf948cad84',
+  expiresIn: '7d',
+};
